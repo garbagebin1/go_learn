e2504c2f52968df6f70fb0db10a4c2e47ae479ec
garbagebin <hekanghui@yeah.net>
2025-03-24 22:49:34 +0800

    init

---
+++ src/hello/hello_test.go
@@ -0,0 +1,14 @@
+package hello
+
+import (
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	got := Hello("Chris")
+	want := "Hello, Chris"
+
+	if got != want {
+		t.Errorf("got '%q' want '%q'", got, want)
+	}
+}
